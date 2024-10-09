0ead2a9d0e7afe0478de637a423c48aafa641aaf
LOLKTKAT <88342619+LOLKTKAT@users.noreply.github.com>
2024-10-09 05:31:51 +0300

    feat(component): added shadCn chart

--- app/componenets/StockChart.tsx
+++ app/componenets/StockChart.tsx
@@ -1,38 +1,65 @@
 "use client";
-import React from "react";
-import { ChartConfig, ChartContainer } from "@/app/componenets/chart";
 
-import { Bar, BarChart } from "recharts";
+import { Area, AreaChart, CartesianGrid, XAxis } from "recharts";
+
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/app/componenets/chart";
+
+export const description = "A linear area chart";
 
 const chartData = [
-  { month: "January", desktop: 186, mobile: 80 },
-  { month: "February", desktop: 305, mobile: 200 },
-  { month: "March", desktop: 237, mobile: 120 },
-  { month: "April", desktop: 73, mobile: 190 },
-  { month: "May", desktop: 209, mobile: 130 },
-  { month: "June", desktop: 214, mobile: 140 },
+  { month: "January", desktop: 186 },
+  { month: "February", desktop: 305 },
+  { month: "March", desktop: 237 },
+  { month: "April", desktop: 73 },
+  { month: "May", desktop: 209 },
+  { month: "June", desktop: 214 },
 ];
 
 const chartConfig = {
   desktop: {
     label: "Desktop",
-    color: "#2563eb",
-  },
-  mobile: {
-    label: "Mobile",
-    color: "#60a5fa",
+    color: "hsl(var(--chart-1))",
   },
 } satisfies ChartConfig;
 
-export function StockChart() {
+function Component() {
   return (
-    <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
-      <BarChart accessibilityLayer data={chartData}>
-        <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
-        <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
-      </BarChart>
+    <ChartContainer config={chartConfig}>
+      <AreaChart
+        accessibilityLayer
+        data={chartData}
+        margin={{
+          left: 12,
+          right: 12,
+        }}
+      >
+        <CartesianGrid vertical={false} />
+        <XAxis
+          dataKey="month"
+          tickLine={false}
+          axisLine={false}
+          tickMargin={8}
+          tickFormatter={(value) => value.slice(0, 3)}
+        />
+        <ChartTooltip
+          cursor={false}
+          content={<ChartTooltipContent indicator="dot" hideLabel />}
+        />
+        <Area
+          dataKey="desktop"
+          type="linear"
+          fill="var(--color-desktop)"
+          fillOpacity={0.4}
+          stroke="var(--color-desktop)"
+        />
+      </AreaChart>
     </ChartContainer>
   );
 }
 
-export default StockChart;
+export default Component;
